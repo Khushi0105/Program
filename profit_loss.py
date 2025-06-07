e18d326b0eacc63f24a1f1986fd294d868b24a2b
unknown <you@example.com>
2025-06-07 15:56:28 +0530

    amount.py

---
+++ profit_loss.py
@@ -0,0 +1,7 @@
+selling_price=int(input("enter the no"))
+cost_price=int(input("enter the no"))
+a=selling_price-cost_price
+if(selling_price-cost_price>=0):
+    print("profit")
+else:
+    print("loss")
