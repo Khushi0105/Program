06b4492c461d6577e4582c8680d8b880c02002f6
unknown <you@example.com>
2025-06-08 15:27:28 +0530

    marri.py

---
+++ marri.py
@@ -0,0 +1,10 @@
+gender1="male"
+gender2="female"
+mariticalstatus="married"
+age=int(input("enter the no"))
+if (gender1=="male"  and mariticalstatus=="married" and age>30):
+    print("you are selected")
+elif (gender2=="female" and mariticalstatus=="married" and age<30):
+   print("selected")
+else:
+    print("not selected")
