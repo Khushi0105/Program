077fadf101b2247fa1b157d05fe7addcb8c9764c
unknown <you@example.com>
2025-06-11 14:02:45 +0530

    monthy.py

---
+++ month.py
@@ -0,0 +1,27 @@
+month=int(input("enter the month no"))
+if(month ==1):
+    print ("month is january,days=31")
+elif(month==2):
+    print("month is febuary,days=28")
+elif(month==3):
+    print("month is march,days=31")
+elif(month==4):
+    print("month is april, days=30")
+elif(month==5):
+    print("month is may, days=31")
+elif(month==6):
+    print("month is june, days=30")
+elif(month==7):
+    print("month is july,days=31")
+elif(month==8):
+    print("month is august, days=31")
+elif(month==9):
+    print("month is september,days=30")
+elif(month==10):
+    print("month is octumber,days=31")
+elif(month==11):
+    print("month is november,days=30")
+elif(month==12):
+    print("month is december,days=31")
+else:
+    print ("not define")
