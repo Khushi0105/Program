c6b15fb8e91b062220af6b560f48b30f8bf5a852
unknown <you@example.com>
2025-06-13 21:39:47 +0530

    side_triangle.py

---
+++ side_triangle.py
@@ -0,0 +1,7 @@
+a=int(input("enter the side"))
+b=int(input("enter the side"))
+c=int(input("enter th side"))
+if (a==b==c):
+    print("triangle is valid")
+else:
+    print("tirangle is not valid")  
